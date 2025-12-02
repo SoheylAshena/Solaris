a19af5079db0313b3251728a9f2cf9d1aefd39f3
Soheyl Ashena <sashena203@gmail.com>
2025-12-02 18:03:48 +0330

    Added rotation animation to star field component

--- src/components/star-field.tsx
+++ src/components/star-field.tsx
@@ -1,55 +1,69 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import type { Points } from "three";
+
 const StarField = ({ count = 1000, minRadius = 50, maxRadius = 200 }) => {
-  const starsCount = count;
+  const pointsRef = useRef<Points>(null);
 
-  const colors = new Float32Array(count * 3);
-  const positions = new Float32Array(count * 3);
-
-  for (let i = 0; i < starsCount; i++) {
-    const theta = Math.random() * 2 * Math.PI;
-    const phi = Math.acos(2 * Math.random() - 1);
-    const radius = minRadius + Math.random() * (maxRadius - minRadius);
-
-    const x = radius * Math.sin(phi) * Math.cos(theta);
-    const y = radius * Math.sin(phi) * Math.sin(theta);
-    const z = radius * Math.cos(phi);
-
-    positions[i * 3] = x;
-    positions[i * 3 + 1] = y;
-    positions[i * 3 + 2] = z;
-
-    let r, g, b;
-    const colorType = Math.random();
-    if (colorType < 0.3) {
-      r = 0;
-      g = 1;
-      b = 1;
-    } else if (colorType < 0.6) {
-      r = 1;
-      g = 1;
-      b = 1;
-    } else if (colorType < 0.9) {
-      r = 1;
-      g = 0.2;
-      b = 0.5;
-    } else {
-      r = 1;
-      g = 0.2;
-      b = 1;
+  useFrame(() => {
+    if (pointsRef.current) {
+      pointsRef.current.rotateX(0.00008);
     }
+  });
 
-    colors[i * 3] = r;
-    colors[i * 3 + 1] = g;
-    colors[i * 3 + 2] = b;
-  }
+  const positions = () => {
+    const pos = new Float32Array(count * 3);
+    for (let i = 0; i < count; i++) {
+      const theta = Math.random() * 2 * Math.PI;
+      const phi = Math.acos(2 * Math.random() - 1);
+      const radius = minRadius + Math.random() * (maxRadius - minRadius);
+
+      pos[i * 3] = radius * Math.sin(phi) * Math.cos(theta);
+      pos[i * 3 + 1] = radius * Math.sin(phi) * Math.sin(theta);
+      pos[i * 3 + 2] = radius * Math.cos(phi);
+    }
+    return pos;
+  };
+
+  const colors = () => {
+    const col = new Float32Array(count * 3);
+    for (let i = 0; i < count; i++) {
+      let r, g, b;
+      const colorType = Math.random();
+      if (colorType < 0.3) {
+        r = 0;
+        g = 1;
+        b = 1;
+      } else if (colorType < 0.6) {
+        r = 1;
+        g = 1;
+        b = 1;
+      } else if (colorType < 0.9) {
+        r = 1;
+        g = 0.2;
+        b = 0.5;
+      } else {
+        r = 1;
+        g = 0.2;
+        b = 1;
+      }
+
+      col[i * 3] = r;
+      col[i * 3 + 1] = g;
+      col[i * 3 + 2] = b;
+    }
+    return col;
+  };
 
   return (
-    <points>
+    <points ref={pointsRef}>
       <bufferGeometry>
-        <bufferAttribute attach={"attributes-position"} args={[positions, 3]} />
-        <bufferAttribute attach={"attributes-color"} args={[colors, 3]} />
+        <bufferAttribute attach="attributes-position" args={[positions(), 3]} />
+        <bufferAttribute attach="attributes-color" args={[colors(), 3]} />
       </bufferGeometry>
-      <pointsMaterial size={0.25} sizeAttenuation vertexColors />
+      <pointsMaterial size={0.25} vertexColors sizeAttenuation />
     </points>
   );
 };
+
 export default StarField;
